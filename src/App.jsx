6e08466a40b2cc9dbd1fd3f822417cf1973397bd
Vinicius Campos <viniciuscampos4@gmail.com>
2025-03-29 12:27:02 -0300

    chore: add InvestmentResult skeleton component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './components/Header/Header';
 import UserInput from './components/User/UserInput';
+import InvestmentResult from './components/InvestmentResult/InvestmentResult';
 function App() {
   return (
     <>
       <Header />
       <UserInput />
+      <InvestmentResult />
     </>
   );
 }
---
+++ src/components/InvestmentResult/InvestmentResult.jsx
@@ -0,0 +1,38 @@
+const InvestmentResult = ({}) => {
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th scope="col">Year</th>
+          <th scope="col">Investment Value</th>
+          <th scope="col">Interest (Year)</th>
+          <th scope="col">Total Interest</th>
+          <th scope="col">Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>a</td>
+          <td>a</td>
+          <td>a</td>
+          <td>a</td>
+          <td>a</td>
+        </tr>
+        <tr>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default InvestmentResult;
